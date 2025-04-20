64b5ab62d4080812e4db16e0e5f6dde5fb0114c6
laroccacharly <larocca.charly@gmail.com>
2025-04-20 18:07:26 -0400

    update main page

--- src/main_ui.py
+++ src/main_ui.py
@@ -5,14 +5,15 @@ from .baseline import baseline
 from .hypertune import hypertune_app
 
 def main_ui():
+    st.set_page_config(page_title="BTC Price Forecasting", layout="wide")
     st.sidebar.title("Navigation")
-    page = st.sidebar.selectbox("Choose a page", ["General View", "Baseline Model", "Log Forecast Model", "Hyperparameter Tuning"], index=2)
+    page = st.sidebar.selectbox("Choose a page", ["General View", "Baseline Model", "Log Model", "Hyperparameter Tuning"], index=3)
 
     if page == "General View":
         general_view()
     elif page == "Baseline Model":
         baseline()
-    elif page == "Log Forecast Model":
+    elif page == "Log Model":
         log_forecast()
     elif page == "Hyperparameter Tuning":
         hypertune_app()
